b140a736904b06ae1a17e3d54529bb57e0974e14
zhenia0908 <eugesha0908@gmail.com>
2024-01-09 20:39:10 +0100

    practice kolos

--- src/main/java/com/zhenia/practicekolos/task2/ProductsPriceComporator.java
+++ src/main/java/com/zhenia/practicekolos/task2/ProductsPriceComporator.java
@@ -6,6 +6,6 @@ public class ProductsPriceComporator implements Comparator<Product> {
     public int compare(Product o1, Product o2){
         int res1 = o1.getPrice();
         int res2 = o2.getPrice();
-        return 
+        return
     }
 }
